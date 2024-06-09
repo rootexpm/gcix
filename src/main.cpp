79b830001cca62ccba27a1ebbb6b76bdb967564a
expm <45915273+rootexpm@users.noreply.github.com>
2024-06-09 08:12:47 +0300

    get rid of setup we dont need it

--- src/main.cpp
+++ src/main.cpp
@@ -6,26 +6,6 @@
 #include "util/console.h"
 #include "patches/patches.h"
 
-//DWORD WINAPI Setup(LPVOID instance)
-//{
-//	//Setup console...
-//	console::Setup();
-//
-//	// Setup hooks...
-//	hooks::Setup();
-//
-//	// Setup patches
-//	patches::Setup();
-//
-//	while (!GetAsyncKeyState(VK_END))
-//		std::this_thread::sleep_for(std::chrono::milliseconds(200));
-//
-//	console::Destroy();
-//	hooks::Destroy();
-//
-//	FreeLibraryAndExitThread(static_cast<HMODULE>(instance), EXIT_SUCCESS);
-//}
-
 BOOL APIENTRY DllMain(HMODULE instance, DWORD reason, LPVOID reserved)
 {
 	if (reason == DLL_PROCESS_ATTACH)
