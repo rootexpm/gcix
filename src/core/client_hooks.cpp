ee5e13afb7d1090a0dbc10acbd8e4eed71d9948c
expm <45915273+rootexpm@users.noreply.github.com>
2024-06-13 01:20:43 +0300

    random shit

--- src/core/client_hooks.cpp
+++ src/core/client_hooks.cpp
@@ -5,6 +5,9 @@
 
 uintptr_t lglobalAddress = 0;
 
+const char* cert = R"(-----BEGIN CERTIFICATE-----
+-----END CERTIFICATE-----)"; // cert here
+
 void client_hooks::Setup(uintptr_t globalAddress) noexcept
 {
 	console::Print("Initializing client hooks: %p", globalAddress);
@@ -41,6 +44,35 @@ void client_hooks::Setup(uintptr_t globalAddress) noexcept
 		reinterpret_cast<void**>(&sslConnectOg)
 	);
 
+	//MH_CreateHook(
+	//	reinterpret_cast<void*>(lglobalAddress + 0x649b60),
+	//	getCertificateHook,
+	//	reinterpret_cast<void**>(&getCertificateOg)
+	//);
+
+	//MH_CreateHook(
+	//	reinterpret_cast<void*>(lglobalAddress + 0x64ac20),
+	//	sub_7B50AC20Hook,
+	//	reinterpret_cast<void**>(&sub_7B50AC20Og)
+	//);
+
+	//MH_CreateHook(
+	//	reinterpret_cast<void*>(lglobalAddress + 0x6a71f0),
+	//	sslReadHook,
+	//	reinterpret_cast<void**>(&sslReadOg)
+	//);
+
+	//MH_CreateHook(
+	//	reinterpret_cast<void*>(lglobalAddress + 0x6a7100),
+	//	sslWriteHook,
+	//	reinterpret_cast<void**>(&sslWriteOg)
+	//);
+
+	// nopping calls below to prevent console being flooded by useless logs
+	mem::Nop((BYTE*)lglobalAddress + 0x64996f, 5);
+	mem::Nop((BYTE*)lglobalAddress + 0x648226, 5);
+	mem::Nop((BYTE*)lglobalAddress + 0x649606, 5);
+
 	MH_EnableHook(MH_ALL_HOOKS);
 
 	console::Print("Client hooks initialized");
@@ -65,9 +97,52 @@ char* __fastcall client_hooks::getBaseServerHook(void* thisPtr, void* Unknown) n
 	return url;
 }
 
+// SSL offset 649b6d
 int __cdecl client_hooks::sslConnectHook(int a1, int a2) noexcept
 {
+	// idek
 	DWORD v6 = a1 + 20 * a2 + 320;
+	*(DWORD*)(v6 + 12) = 10;
 	*(DWORD*)(v6 + 16) = 4;
+	int result = sslConnectOg(a1, a2);
+	return result;
+}
+
+int __cdecl client_hooks::getCertificateHook(int a1) noexcept
+{
+	if (a1 == 0)
+		return 0;
+
+	int v5 = 1;
+	const char* Str = cert;
+
+	while (v5 == 1 && Str)
+	{
+		size_t v1 = std::strlen(Str);
+		v5 = sub_7B50AC20Og(a1, Str, v1, 1);
+		if (v5 == 1)
+		{
+			const char* v2 = std::strstr(Str, "-----END CERTIFICATE-----");
+			Str = std::strstr(v2, "-----BEGIN CERTIFICATE-----");
+		}
+	}
+	return v5;
+}
+
+int __cdecl client_hooks::sub_7B50AC20Hook(int a1, const char* a2, size_t a3, int a4) noexcept
+{
 	return 0;
+}
+
+int __cdecl client_hooks::sslReadHook(void* a1, int a2, void* a3, unsigned int a4, void* a5) noexcept
+{
+
+	a5 = 0;
+	return a4;
+}
+
+int __cdecl client_hooks::sslWriteHook(void* a1, int a2, int a3, unsigned int a4, void* a5) noexcept
+{
+	a5 = 0;
+	return a4;
 }
--- src/core/client_hooks.h
+++ src/core/client_hooks.h
@@ -17,4 +17,20 @@ namespace client_hooks
 	using sslConnectFn = int(__cdecl*)(int, int) noexcept;
 	inline sslConnectFn sslConnectOg = nullptr;
 	int __cdecl sslConnectHook(int a1, int a2) noexcept;
+
+	using getCertificateFn = int(__cdecl*)(int) noexcept;
+	inline getCertificateFn getCertificateOg = nullptr;
+	int __cdecl getCertificateHook(int a1) noexcept;
+
+	using sub_7B50AC20Fn = int(__cdecl*)(int, const char*, size_t, int) noexcept;
+	inline sub_7B50AC20Fn sub_7B50AC20Og = nullptr;
+	int __cdecl sub_7B50AC20Hook(int a1, const char* a2, size_t a3, int a4) noexcept;
+
+	using sslReadFn = int(__cdecl*)(void*, int, void*, unsigned int, void*) noexcept;
+	inline sslReadFn sslReadOg = nullptr;
+	int __cdecl sslReadHook(void* a1, int a2, void* a3, unsigned int a4, void* a5) noexcept;
+
+	using sslWriteFn = int(__cdecl*)(void*, int, int, unsigned int, void*) noexcept;
+	inline sslWriteFn sslWriteOg = nullptr;
+	int __cdecl sslWriteHook(void* a1, int a2, int a3, unsigned int a4, void* a5) noexcept;
 }
